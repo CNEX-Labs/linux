c10e58298f1c1b6ad68c117d7c02ca1691170f1c
Hans Holmberg <hans.holmberg@cnexlabs.com>
2019-01-10 13:53:48 +0100

    always remap before invalidate

--- drivers/lightnvm/pblk-core.c
+++ drivers/lightnvm/pblk-core.c
@@ -304,15 +304,16 @@ static void pblk_invalidate_range(struct pblk *pblk, sector_t slba,
 
 	spin_lock(&pblk->trans_lock);
 	for (lba = slba; lba < slba + nr_secs; lba++) {
-		struct ppa_addr ppa;
+		struct ppa_addr l2p_ppa, ppa;
 
-		ppa = pblk_trans_map_get(pblk, lba);
-
-		if (!pblk_addr_in_cache(ppa) && !pblk_ppa_empty(ppa))
-			pblk_map_invalidate(pblk, ppa);
+		l2p_ppa = pblk_trans_map_get(pblk, lba);
 
 		pblk_ppa_set_empty(&ppa);
 		pblk_trans_map_set(pblk, lba, ppa);
+
+		if (!pblk_addr_in_cache(l2p_ppa) && !pblk_ppa_empty(l2p_ppa))
+			pblk_map_invalidate(pblk, l2p_ppa);
+
 	}
 	spin_unlock(&pblk->trans_lock);
 }
@@ -2070,10 +2071,11 @@ void pblk_update_map(struct pblk *pblk, sector_t lba, struct ppa_addr ppa)
 	spin_lock(&pblk->trans_lock);
 	ppa_l2p = pblk_trans_map_get(pblk, lba);
 
+	pblk_trans_map_set(pblk, lba, ppa);
+
 	if (!pblk_addr_in_cache(ppa_l2p) && !pblk_ppa_empty(ppa_l2p))
 		pblk_map_invalidate(pblk, ppa_l2p);
 
-	pblk_trans_map_set(pblk, lba, ppa);
 	spin_unlock(&pblk->trans_lock);
 }
 
